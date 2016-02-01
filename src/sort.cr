2686c9359d1150555731232491ad232123fb6095
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-01 18:45:54 +0100

    [GREEN] Change 106

--- src/sort.cr
+++ src/sort.cr
@@ -1,13 +1,11 @@
 module Sort
   def self.sort(a)
     i = 0
-    if a.size > i + 1 && a[i] > a[i + 1]
-      a[i], a[i + 1] = {a[i + 1], a[i]}
-    end
-
-    i = 1
-    if a.size > i + 1 && a[i] > a[i + 1]
-      a[i], a[i + 1] = {a[i + 1], a[i]}
+    while a.size > i + 1
+      if a.size > i + 1 && a[i] > a[i + 1]
+        a[i], a[i + 1] = {a[i + 1], a[i]}
+      end
+      i += 1
     end
 
     a
