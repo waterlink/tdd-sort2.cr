57d0ac08a180a4f56e16415800608f51e1077a1c
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-01 18:48:47 +0100

    [GREEN] Change 111

--- src/sort.cr
+++ src/sort.cr
@@ -1,14 +1,10 @@
 module Sort
   def self.sort(a)
-    (a.size - 1).times do |i|
-      if a[i] > a[i + 1]
-        a[i], a[i + 1] = {a[i + 1], a[i]}
-      end
-    end
-
-    (a.size - 1).times do |i|
-      if a[i] > a[i + 1]
-        a[i], a[i + 1] = {a[i + 1], a[i]}
+    2.times do
+      (a.size - 1).times do |i|
+        if a[i] > a[i + 1]
+          a[i], a[i + 1] = {a[i + 1], a[i]}
+        end
       end
     end
 
