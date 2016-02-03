1c48970cd33495b56f898e285ee478566b2f7741
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-03 23:00:12 +0100

    [GREEN] Change 265

--- spec/kind_sort_spec.cr
+++ spec/kind_sort_spec.cr
@@ -10,4 +10,10 @@ describe KindSort do
     KindSort.sort([1, 2]).should eq([1, 2])
     KindSort.sort([2, 1]).should eq([1, 2])
   end
+
+  it "does not modify original array" do
+    a = [2, 1]
+    KindSort.sort(a).should eq([1, 2])
+    a.should eq([1, 2])
+  end
 end
