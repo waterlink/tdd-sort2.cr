c505a0ecbe6b820461bf5fcc8771d1a3270f78a6
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-03 23:07:03 +0100

    [GREEN] Change 277

--- src/kind_sort.cr
+++ src/kind_sort.cr
@@ -3,10 +3,10 @@ module KindSort
     return a if a.size < 1
 
     if a.size > 1 && a[0] > a[1]
-      return [a[1], a[0]] + a[2..-1]
+      return [a[1]] + [a[0]] + a[2..-1]
     end
 
-    [a[0]] + sort(a[1..-1])
+    empty + [a[0]] + sort(a[1..-1])
   end
 
   private def self.empty
