5564f6329705150d6ba3db42a914aff8dfed3d7b
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-03 23:18:43 +0100

    [GREEN] Change 299

--- spec/kind_sort_spec.cr
+++ spec/kind_sort_spec.cr
@@ -15,6 +15,9 @@ describe KindSort do
     KindSort.sort([1, 3, 2]).should eq([1, 2, 3])
     KindSort.sort([2, 3, 1]).should eq([1, 2, 3])
     KindSort.sort([3, 2, 1]).should eq([1, 2, 3])
+
+    KindSort.sort([55, 60, 63, 95, 87, 75, 9, 86, 50, 80, 3, 82, 37, 67])
+      .should eq([3, 9, 37, 50, 55, 60, 63, 67, 75, 80, 82, 86, 87, 95])
   end
 
   it "does not modify original array" do
